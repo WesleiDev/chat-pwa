8aa3b909403cab68f348621a4c6e7d11211d3647
Weslei <weslei.fs@hotmail.com>
2018-11-02 17:22:35 -0300

    Melhorando layout do sidebar

--- src/talk/index.js
+++ src/talk/index.js
@@ -5,11 +5,22 @@ export default {
     template: require('./template.html'),
     afteBind :()=>{
         console.log('Carregou a conversa')
-        const talk    = document.getElementById('talk');
+        const talk       = document.getElementById('talk');
         const sideBar    = document.getElementById('sidebar');
+        const tabs       = document.querySelectorAll('.tab');
+
+
         talk.addEventListener('click', function(e){
             e.preventDefault();
             sideBar.className = 'hide-sidebar';
+        })
+
+        tabs.forEach((e, index)=>{
+            tabs[index].addEventListener('click', function(e){
+                const currentTabActive = document.querySelector('.tab-active')
+                currentTabActive.className = 'tab';
+                e.currentTarget.className  = 'tab tab-active';
+            })
         })
     }
 }
