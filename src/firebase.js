4238f6e79e69a916f79bfd516c3ebfa19f38fd37
Weslei <weslei.fs@hotmail.com>
2018-11-05 21:23:36 -0200

    Iniciando Firebase no projeto

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+import * as firebase from 'firebase';
+
+export  const app = firebase.initializeApp({
+  apiKey: "AIzaSyDUpaiCXn5y3a0wwbhyI5LRAQ2w7vP6qO0",
+  authDomain: "chat-pwa-weslei.firebaseapp.com",
+  databaseURL: "https://chat-pwa-weslei.firebaseio.com",
+  projectId: "chat-pwa-weslei",
+  storageBucket: "chat-pwa-weslei.appspot.com",
+  messagingSenderId: "578228694044"
+}) ;
+  
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import TemplateComponent from './template';
 import TalkComponent from './talk';
+import { app } from './firebase'
 
 const components = [
     TemplateComponent,
@@ -12,7 +13,66 @@ class Init{
             let element = document.querySelector(component.el);
             element.innerHTML =component.template;
             component.afteBind();
-        })      
+        })     
+        
+        // let ref = app.database().ref('/posts/messages/1/2');
+    
+        // ref.set({
+        //     message:"Bate o pé você",
+        //     emitter: 4,
+        //     destinatorio: 3
+        // });
+
+        // let ref = app.database().ref('/users/1');
+    
+        // ref.set({
+        //     name:"Weslei Ferreira",
+        //     foto: 'URL DA FOTO PERFIL AQUI',
+        // });
+
+        // let ref = app.database().ref('/list_posts/1/1');
+    
+        // ref.set({
+        //     user_id:2
+        // });
+
+        let ref = app.database().ref('/list_posts/1');
+    
+        // ref.set({
+        //     user_id:3
+        // });
+
+        // ref.on('value', function(snapshot){
+        //     console.log(snapshot.val());
+        // })
+
+        var myConnectionsRef = app.database().ref('users/joe/connections');
+
+        var lastOnlineRef = app.database().ref('users/joe/lastOnline');
+
+        var connectedRef = app.database().ref('.info/connected');
+        connectedRef.on('value', function(snap) {
+        
+        if (snap.val() === true) {
+            console.log('Disconected')
+            console.log(snap)
+            // We're connected (or reconnected)! Do anything here that should happen only if online (or on reconnect)
+            var con = myConnectionsRef.push();
+
+            // When I disconnect, remove this device
+            con.onDisconnect().remove();
+
+            // Add this device to my connections list
+            // this value could contain info about the device or a timestamp too
+            con.set(true);
+
+            lastOnlineRef.onDisconnect().set({
+                desc: 1
+            });
+
+    
+        }
+        });
     
     }
 }
