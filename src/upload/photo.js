ed27be7a9f84952b3ca62bd8c7454824d7cdb71f
Weslei <weslei.fs@hotmail.com>
2018-12-06 22:17:32 -0200

    Adicionado template para rotina de tirar foto

--- src/upload/photo.js
+++ src/upload/photo.js
@@ -1,3 +1,5 @@
+let partialOption = require('./photo/template.html');
+
 export default function(){
     //Carrega todos os dispositivos conectados
 navigator.mediaDevices.enumerateDevices().then((devices) => {
@@ -5,10 +7,29 @@ navigator.mediaDevices.enumerateDevices().then((devices) => {
 
 
     devices.forEach((device)=>{
-        console.log(device);
+         console.log(device);
+        if(device.kind == 'videoinput'){
+            options.push(device);
+        } 
+        // options.push(device);    
     })
 
+    // let template =
+
     //Cria as opções de camera
+    let optionsPhoto       = document.getElementById('options-photo');
+    let html = '';
+
+
+    options.forEach((device) => {
+        let label =  device.label;
+        html += partialOption
+                .replace(/{{ id }}/g, device.deviceId)
+                .replace(/{{ label }}/g, label)    
+    })
+
+    console.log(html)
+    optionsPhoto.innerHTML = html;
 
 
 })
